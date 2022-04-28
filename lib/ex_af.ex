ecdc6614e4ecf7f981dc795d018dd618edccc534
Benjamin Philip <benjamin.philip495@gmail.com>
2022-04-28 08:37:44 +0530

    Initialize Project

---
+++ lib/ex_af.ex
@@ -0,0 +1,18 @@
+defmodule ExAF do
+  @moduledoc """
+  Documentation for `ExAF`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExAF.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_af_test.exs
@@ -0,0 +1,8 @@
+defmodule ExAFTest do
+  use ExUnit.Case
+  doctest ExAF
+
+  test "greets the world" do
+    assert ExAF.hello() == :world
+  end
+end
